b2b7514c2aba4b9ed32b91972745a92a80e31f66
Jen Volpe <jvolpe721@gmail.com>
2018-01-31 13:25:04 -0500

    added form validations

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var submitButton = document.getElementById("submit");
-var email = document.getElementById("auto");
-var error = document.querySelector(".error");
 var buttons = document.querySelectorAll("a.large.button");
+var form = document.getElementById("flex-grid-form");
+var successMessage = document.getElementById("successMessage");
 
 buttons.forEach(button => {
   button.addEventListener("click", function(ev) {
@@ -11,4 +11,21 @@ buttons.forEach(button => {
 
 submitButton.addEventListener("click", event => {
   event.preventDefault();
+  check(event);
 });
+
+function check(event) {
+  if (zipCode.value === "" || email.value === "") {
+    errorMessage.innerText =
+      "Please enter a valid zip code and/or email address.";
+  } else {
+    errorMessage.innerText = "";
+    form.style.display = "none";
+    successMessage.style.display = "block";
+    successMessage.innerText = "Thank you for submittng!";
+  }
+}
+
+var email = document.getElementById("email");
+var zipCode = document.getElementById("zip-code");
+var errorMessage = document.getElementById("errorname");
